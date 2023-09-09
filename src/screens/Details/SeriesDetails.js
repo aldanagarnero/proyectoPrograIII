d9ac6f8650fd6311d764e31f6213b16962af0a24
Sguntin <sguntin@udesa.edu.ar>
2023-09-09 16:35:25 -0300

    details estructura + gif + notfound

--- src/screens/Details/SeriesDetails.js
+++ src/screens/Details/SeriesDetails.js
@@ -1,2 +1,30 @@
-import React from 'react';
+import React, {Component} from 'react';
 import './Details.css';
+
+class SeriesDetails extends Component{
+    constructor(){
+        super()
+        this.state = {
+            series: []
+        }
+    }
+
+    componentDidMount(){
+        fetch('https://api.themoviedb.org/3/tv/series_id?api_key=0317bbf7efac7dd04b2c2c3748377d57&language=en-US&page=1')
+            .then(response => response.json())
+            .then(data => this.setState(
+                {series: data.results} 
+            ))
+            .catch(e => console.log(e));
+    }
+    render(){
+        console.log(this.state)
+        return(
+            <section>
+                
+            </section>
+        );
+    };
+}
+
+export default SeriesDetails
---
+++ src/screens/NotFound/NotFound.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+function NotFound(){
+    return(
+        <p className="texto">Error 404 Not Found</p>
+    )
+}
+export default NotFound
