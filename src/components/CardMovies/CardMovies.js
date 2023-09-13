5039ff9b5312408c7de9e2c5333db5e80addc8a6
aldanagarnero <agarnero@udesa.edu.ar>
2023-09-13 15:44:58 -0300

    favoritos

--- src/components/CardMovies/CardMovies.js
+++ src/components/CardMovies/CardMovies.js
@@ -1,22 +1,23 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import './CardMovies.css';
-import {Link} from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
-class CardMovies extends Component{
-    constructor(props){
+class CardMovies extends Component {
+    constructor(props) {
         super(props);
         this.state = {
-            text:'Agregar a favoritos',
+            text: 'Agregar a favoritos',
         }
     }
 
-    componentDidMount(){
+    componentDidMount() {
         let favorites = []
         let recuperoStorage = localStorage.getItem('favorites');
+        if (recuperoStorage !== null) {
 
-        if (recuperoStorage !== null){favorites = JSON.parse(recuperoStorage);
+            favorites = JSON.parse(recuperoStorage);
 
-            if(favorites.includes(this.props.movieData.id)){ 
+            if (favorites.includes(this.props.movieData.id)) {
                 this.setState({
                     text: 'Quitar de favoritos'
                 })
@@ -24,18 +25,18 @@ class CardMovies extends Component{
         }
     }
 
-    functionFavs(id){
+    functionFavs(id) {
         let favorites = []
         let recuperoStorage = localStorage.getItem('favorites');
 
-        if (recuperoStorage !== null){
+        if (recuperoStorage !== null) {
             favorites = JSON.parse(recuperoStorage);
         }
 
-        if(favorites.includes(id)){
-            favorites = favorites.filter( unId => unId !== id);
+        if (favorites.includes(id)) {
+            favorites = favorites.filter(unId => unId !== id);
             this.setState({
-                text : 'Agregar a favoritos'
+                text: 'Agregar a favoritos'
             })
 
         } else {
@@ -46,22 +47,22 @@ class CardMovies extends Component{
         }
 
         //Guardar en localStorage
-        let favoritesToString = JSON.stringify(favorites);        
+        let favoritesToString = JSON.stringify(favorites);
         localStorage.setItem('favorites', favoritesToString);
     }
 
-    render(){
+    render() {
         return (
             <div className='characterCard'>
                 <Link to={`/moviedetails/id/${this.props.movieData.id}`}>
-                    <img src={`https://image.tmdb.org/t/p/w300/${this.props.movieData.poster_path}`} alt={this.props.movieData.original_title}/>
+                    <img src={`https://image.tmdb.org/t/p/w300/${this.props.movieData.poster_path}`} alt={this.props.movieData.original_title} />
                 </Link>
 
                 <Link to={`/moviedetails/id/${this.props.movieData.id}`}>
                     <h2>{this.props.movieData.title}</h2>
                 </Link>
 
-                <button onClick={()=>this.functionFavs(this.props.movieData.id)} type='button'>{this.state.text}</button>
+                <button onClick={() => this.functionFavs(this.props.movieData.id)} type='button'>{this.state.text}</button>
 
             </div>
         )
--- src/screens/Favorites/Favorites.js
+++ src/screens/Favorites/Favorites.js
@@ -1,47 +1,49 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import './Favorites.css';
 import CardMovies from '../../components/CardMovies/CardMovies';
 
 class Favorites extends Component {
-    constructor() {
-      super();
-      this.state = {
-      };
-    }  
-  
-    componentDidMount() {
-      let favorites = []
-      let recuperoStorage = localStorage.getItem('favorites');
-
-      if (recuperoStorage !== null){
-        favorites = JSON.parse(recuperoStorage);
-      }
-
-      favorites.forEach(id => {
-        
-      });
-
-      fetch(`https://api.themoviedb.org/3/movie/upcoming?api_key=a3c55e0abc72e6abaa573f83ee40635f&language=en-US&page=1`)
-        .then((response) => response.json())
-        .then((data) =>{
-          this.setState({
-            upcoming : data.results
-          })
-        })
-        .catch((error) => console.log(error));
-      }
-  
-    render() {
-      return(
-        <React.Fragment> 
-          <main>
-            <h2>PELICULAS FAVORITAS</h2>
-            
-          </main>
-        </React.Fragment>
-
-      )
+  constructor() {
+    super();
+    this.state = {
+      favorites: []
     }
   }
+
+  componentDidMount() {
+    //Recupero el storage
+    let favoritesMovies = []
+    let recuperoStorage = localStorage.getItem('favorites');
+
+    if (recuperoStorage !== null) {
+      favoritesMovies = JSON.parse(recuperoStorage);
+      let array = [];
+
+      favoritesMovies.forEach((id) => {
+        fetch(`https://api.themoviedb.org/3/movie/${id}?api_key=0317bbf7efac7dd04b2c2c3748377d57&language=en-US`)
+          .then((response) => response.json())
+          .then((favoritesMovies) => {
+            array.push(favoritesMovies);
+            this.setState({
+              favorites: array
+            })
+          })
+          .catch((error) => console.log(error));
+
+      })
+    };
+  }
+
+  render() {
+    return (
+      <React.Fragment>
+        <main>
+          <h2>PELICULAS FAVORITAS</h2>
+          {this.state.favorites.length > 0 ? <CardMovies favorites={this.state.favorites} /> : <h3>No seleccionaste películas favoritas</h3>}
+        </main>
+      </React.Fragment>
+    )
+  }
+}
 
 export default Favorites;
